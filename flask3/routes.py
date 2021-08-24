99f2ada14e4d3e48834774e0d76aa81feab9936c
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-24 18:03:29 +0300

    random quotes

--- flask3/routes.py
+++ flask3/routes.py
@@ -6,3 +6,12 @@ from flask3 import app,db,bcrypt
 from flask3.forms import RegistrationForm, LoginForm,UpdateAccountForm,PostForm,CommentsForm
 from flask3.models import User,Post,Comment,Quote
 from flask_login import login_user,current_user,logout_user,login_required
+
+quotes=[
+    {
+        "author": "Brian Kernighan",
+        "id": 5,
+        "quote": "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+        "permalink": "http://quotes.stormconsultancy.co.uk/quotes/5"
+    }
+ ]
