d3c790b18c86be435494e8e707edc8ca54c51cd8
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-24 18:05:29 +0300

    about route

--- flask3/routes.py
+++ flask3/routes.py
@@ -23,3 +23,6 @@ def home():
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
    
     return render_template('home.html',posts= posts)
+@app.route('/about')
+def about():
+    return render_template('about.html',title= "about")
