d1e144f4e995d4c619192c1545aa57a70951b3ba
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-24 17:59:45 +0300

    class comment in models

--- flask3/models.py
+++ flask3/models.py
@@ -30,3 +30,13 @@ class Quote:
     def __init__ (self,author,quote):
         self.author = author
         self.quote = quote
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
