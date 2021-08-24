a22347efda84c16ecb1f61ce6c7c14e46fff6e9e
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-24 17:47:39 +0300

    class loginForm

--- flask3/forms.py
+++ flask3/forms.py
@@ -23,3 +23,11 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
