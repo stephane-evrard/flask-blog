7d0e4d7f9a9011a8ce5a222b4f9e7a4ac4ca03e8
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-24 18:07:45 +0300

    login route

--- flask3/routes.py
+++ flask3/routes.py
@@ -41,3 +41,20 @@ def register():
        return redirect(url_for('login'))
 
    return render_template('register.html',title="register", form= form)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+  if current_user.is_authenticated:
+      return redirect(url_for('home'))
+  form =LoginForm()
+  if form.validate_on_submit():
+      user = User.query.filter_by(email=form.email.data).first()
+      if user and bcrypt.check_password_hash(user.password, form.password.data):
+         login_user(user, remember=form.remember.data)
+         next_page = request.args.get('next')
+         return  redirect(next_page) if next_page else redirect(url_for('home'))
+       
+      else:       
+          flash('Login Unsuccessful. Please check username and password', 'danger')
+
+  return render_template('login.html',title="Login", form= form)
