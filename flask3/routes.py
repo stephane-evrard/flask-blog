472021e6e4d4189ca29e622204658e6b2fe988e0
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-24 18:06:28 +0300

    register route

--- flask3/routes.py
+++ flask3/routes.py
@@ -26,3 +26,18 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title= "about")
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+   if current_user.is_authenticated:
+       return redirect(url_for('home'))
+   form =RegistrationForm()
+   if form.validate_on_submit():
+       hashed_password =bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+       user= User(username=form.username.data, email=form.email.data, password=hashed_password)
+       db.session.add(user)
+       db.session.commit()
+       flash('Your account has been created! You are now able to log in', 'success')
+       return redirect(url_for('login'))
+
+   return render_template('register.html',title="register", form= form)
